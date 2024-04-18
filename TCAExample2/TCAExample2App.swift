24aef6afc6ba907bed0b7be19434eb6b54fe2fc6
Abdulrahman Qabbout <abdulrahman.qabbout@gmail.com>
2024-04-18 15:02:04 +0300

    Initial Commit

---
+++ TCAExample2/TCAExample2App.swift
@@ -0,0 +1,17 @@
+//
+//  TCAExample2App.swift
+//  TCAExample2
+//
+//  Created by Abdulrahman Qabbout on 18/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct TCAExample2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
